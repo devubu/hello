ed556ca3c7cc9ca4c25a70526b4fd6357486fa54
devubu <159164402+devubu@users.noreply.github.com>
2024-12-30 11:27:15 -0500

    Add files via upload

---
+++ lua/hello/README.md
@@ -0,0 +1,5 @@
+## Install package (Specify the version)
+    sudo apt install lua5.4
+
+## Run the Lua script
+    lua hello.lua
---
+++ lua/hello/hello.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/env lua
+
+local function say_hello()
+    print("Hello, world!")
+end
+
+local function main()
+    say_hello()
+end
+
+main()
